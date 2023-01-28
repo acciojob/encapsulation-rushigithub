17a66872e36535d2ffd99d5f11a0fd40a157740b
rushigithub <gaderushi89@gmail.com>
2023-01-28 10:43:06 +0530

    solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,14 @@ package com.driver;
 
 public class Main {
   
+}package com.driver;
+
+public class Main {
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        obj.setName("Rushi");
+        obj.getName();
+
+    }
+
 }
